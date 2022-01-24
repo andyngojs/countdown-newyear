ff7db42378b1ad9e282716a755cc6a11477730d5
andyngojs <dongngo.2000@gmail.com>
2022-01-24 08:51:19 +0700

    refactor: remove handlemusic into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,23 @@ const newYearLunar = document.querySelector(".newyear");
 const musicPlay = document.querySelector(".musics");
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
+const songs = [
+  {
+      name: "Năm qua tôi đã làm gì",
+      path: './assets/music/NamQuaToiDaLamGi.mp3',
+  },
+  {
+      name: "1 năm mới bình an",
+      path: './assets/music/1nammoibinhan.mp3',
+  },
+  {
+      name: "Happy New Year",
+      path: "./assets/music/HappyNewYear-ABBA.mp3"
+  }
+];
+let isPlaying = false
+let currSong, flag = 0
+const audio = new Audio()
 
 const firework = new Fireworks(fireWorkContainer, {
   speech: 4,
@@ -53,6 +70,57 @@ const countdown = () => {
     clearInterval(countdownInterval);
   }
 };
+
+const handleEvent = () => {
+  musicPlay.addEventListener("click", (e) => {
+      if (isPlaying) {
+          playBtn.classList.remove("hide")
+          pauseBtn.classList.add("hide")
+          currSong.pause()
+      } else {
+          playBtn.classList.add("hide")
+          pauseBtn.classList.remove("hide")
+          pauseBtn.style.animation = 'rotateBtn 2s linear 0.2s infinite'
+          selectTrack(flag)
+      }
+  })
+}
+
+function  selectTrack(flag) {
+  songs.forEach((song, index) => {
+      if (flag === index) {
+          initPlayer(song.path, (flag) => {
+              selectTrack(flag)
+          });
+      } 
+  })
+}
+
+function initPlayer(url, callback) {
+  audio.src = url
+  if (audio.src !== undefined) {
+      audio.play()
+  }
+  audio.volume = 0.3
+  audio.onplay = () => {
+      isPlaying = true
+      currSong = audio
+  }
+  audio.onpause = () => {
+      isPlaying = false
+      currSong = null
+  }
+  audio.onended = () => {
+      isPlaying = false
+      currSong = null
+      if (flag < songs.length - 1 ) {
+          callback(++flag)
+      } else {
+          flag = 0
+          callback(flag)
+      }
+  }
+}
 
 firework.start();
 countdown();
